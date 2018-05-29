8ef33c8c6a2254f69c0f42deee55d6760297b4ce
Jake <jakemacnaughton@gmail.com>
2018-05-29 10:10:19 -0400

    removed solutions

--- models/user_show.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import *
-from base import Base
-
-class UserShow(Base):
-    __tablename__ = 'user_shows'
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-    show_id = Column(Integer, ForeignKey('shows.id'), primary_key=True)
